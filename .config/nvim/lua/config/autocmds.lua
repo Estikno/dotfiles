924b65aa1b826cf2edf4500cd9c62de2e3e6c004
Estikno <95084542+Estikno@users.noreply.github.com>
2026-06-20 13:19:26 +0200

    Add Omarchy config

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,28 +1,8 @@
--- Eliminates padding when running in an Alacritty terminal
-
--- Only run if inside Alacritty
-if vim.env.ALACRITTY_WINDOW_ID then
-  local function set_padding(x, y)
-    vim.fn.jobstart({
-      "alacritty",
-      "msg",
-      "config",
-      "window.padding.x=" .. x,
-      "window.padding.y=" .. y,
-    }, { detach = true })
-  end
-
-  -- Remove padding on enter
-  vim.api.nvim_create_autocmd("VimEnter", {
-    callback = function()
-      set_padding(0, 0)
-    end,
-  })
-
-  -- Restore padding on exit
-  vim.api.nvim_create_autocmd("VimLeavePre", {
-    callback = function()
-      set_padding(5, 5) -- <- change to your default padding
-    end,
-  })
-end
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
--- .config/qtile/autostart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-picom &
-dunst &
-udiskie &
